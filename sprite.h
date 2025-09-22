93a6f6845022850f7e3829b099b645103b3ced42
CmdrA43 <144743060+CmdrA43@users.noreply.github.com>
2025-09-22 12:57:04 -0700

    Create sprite.h
    Starting building for spritesheets and animation sprite for 2d

---
+++ sprite.h
@@ -0,0 +1,19 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+namespace frame{
+    class spritesheet{
+        public:
+        int sizeX;
+        int sizeY;
+        int splitX;
+        int splitY;
+        char[50] name;
+        int numImgs = (sizeX / splitX) * (sizeY / splitY);
+    }
+    class sprite{
+        public:
+        spritesheet sheet;
+        char[50] state;
+    }
+}
