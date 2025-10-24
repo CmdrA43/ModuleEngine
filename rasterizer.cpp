6a9100d6a32af22eb684995fbf047768a23e40ed
CmdrA43 <144743060+CmdrA43@users.noreply.github.com>
2025-10-24 09:24:39 -0700

    Update rasterizer.cpp
    Adding render bounds

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,4 +1,5 @@
 bool boundCheck(obj camera, vector3 point){
-  vector3 dir = camera.pos.normalize();
-  
+  vector3 dir = camera.rotation.normalize();
+  vector3 lBound = arctan(camera.rotation.y/camera.rotation.x) + 0.785398163;
+  vector3 rBound = arctan(camera.rotation.y/camera.rotation.x) - 0.785398163;
 }
