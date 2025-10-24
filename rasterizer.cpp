1408a34c0c256f6cd6167bf3e8d21f4370feae73
CmdrA43 <144743060+CmdrA43@users.noreply.github.com>
2025-10-24 12:14:27 -0700

    Update rasterizer.cpp
    Finished adding bounds (only currently work for rotation along z-axis)

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,5 +1,7 @@
 bool boundCheck(obj camera, vector3 point){
   vector3 dir = camera.rotation.normalize();
-  vector3 lBound = arctan(camera.rotation.y/camera.rotation.x) + 0.785398163;
-  vector3 rBound = arctan(camera.rotation.y/camera.rotation.x) - 0.785398163;
+  double leftBound = arctan(camera.rotation.y/camera.rotation.x) + 0.785398163;
+  double rightBound = arctan(camera.rotation.y/camera.rotation.x) - 0.785398163;
+  double upBound = (camera.rotation.y + camera.rotation.x) * 0.588002604;
+  double downBound = (camera.rotation.y + camera.rotation.x) * (0-0.588002604);
 }
