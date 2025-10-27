872918599d75dc04dfb8b4c3d0149f5c5d9ef64d
CmdrA43 <144743060+CmdrA43@users.noreply.github.com>
2025-10-27 09:48:15 -0700

    Update rasterizer.cpp
    Fixing equations and abstracting some steps

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,7 +1,19 @@
-bool boundCheck(obj camera, vector3 point){
+//camera rotation should be the sin and cos of the unit vector for x and y in radians
+
+bool vertBoundChecks(obj camera, vector3 point){
   vector3 dir = camera.rotation.normalize();
-  double leftBound = arctan(camera.rotation.y/camera.rotation.x) + 0.785398163;
-  double rightBound = arctan(camera.rotation.y/camera.rotation.x) - 0.785398163;
-  double upBound = (camera.rotation.y + camera.rotation.x) * 0.588002604;
-  double downBound = (camera.rotation.y + camera.rotation.x) * (0-0.588002604);
+  if((((point.y * dir.y) + (point.x * dir.x)) * 0.588002604) > point.z && (((point.y * dir.y) + (point.x * dir.x)) * -0.588002604) < point.z){
+    return true;
+  }
+  return false;
+}
+
+bool boundsCheck(obj camera, vector3 point){
+  vector3 dir = camera.rotation.normalize();
+  double leftBound = tan(arctan(dir.y/dir.x) + 0.785398163);
+  double rightBound = tan(arctan(dir.y/dir.x) - 0.785398163);
+  if(vertBoundChecks(camera, point) && (leftBound * point.x) > point.y && (rightBound * point.x) < point.y){
+    return true;
+  }
+  return false;
 }
